80081bd9658ece9965b834dc4f8421d3070f13a0
yosra <chekiryosra99@gmail.com>
2022-12-10 19:05:35 +0100

    First commit Micro forum

---
+++ MicroForum/src/test/java/com/winners/microforum/MicroForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winners.microforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
